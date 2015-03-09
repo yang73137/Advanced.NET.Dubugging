7515301c047336fd33bebc8d63968da5767559e5
Leon <yang73137@126.com>
2015-03-09 23:28:15 +0800

    Add new project.

---
+++ Advanced .NET Debugging/Advanced.NET.Debugging.Chapter5/Finalize.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced.NET.Debugging.Chapter5
+{
+    public class Finalize
+    {
+        public class NativeEvent
+        {
+            public IntPtr NativeHandle { get; private set; }
+
+            public NativeEvent(string name)
+            {
+                NativeHandle = CreateEvent(IntPtr.Zero, false, true, name);
+            }
+
+            ~NativeEvent()
+            {
+                if (this.NativeHandle != IntPtr.Zero)
+                {
+                    CloseHandle(this.NativeHandle);
+                    this.NativeHandle = IntPtr.Zero;
+                }
+            }
+
+            [DllImport("kernel32.dll")]
+            private static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string lpName);
+
+            [DllImport("kernel32.dll")]
+            private static extern IntPtr CloseHandle(IntPtr lpEvent);
+        }
+
+        public void Run()
+        {
+            NativeEvent nEvent = new NativeEvent("MyNewEvent");
+
+            //
+            // 使用 nEvent
+            //
+
+            nEvent = null;
+
+            Console.WriteLine("Pressa any key to GC");
+            Console.ReadKey();
+
+            GC.Collect();
+
+            Console.WriteLine("Pressa any key to GC");
+            Console.ReadKey();
+
+            GC.Collect();
+
+            Console.WriteLine("Pressa any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Advanced .NET Debugging/Advanced.NET.Debugging.Chapter5/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Advanced.NET.Debugging.Chapter5
+{
+    internal class Name
+    {
+        public string First { get; private set; }
+
+        public string Last { get; private set; }
+
+        public Name(string first, string last)
+        {
+            First = first;
+            Last = last;
+        }
+    }
+
+    class Program
+    {
+        public static Name CompleteName = new Name("First", "Last");
+
+        private Thread thread;
+        private bool shouldExit;
+
+        static void Main(string[] args)
+        {
+            Finalize finalize = new Finalize();
+            finalize.Run();
+        }
+
+        public void Gen()
+        {
+            Name n1 =  new Name("Mario", "Hewardt");
+            Name n2 = new Name("Mario", "Hewardt");
+
+            Console.WriteLine("Allocated objects");
+
+            Console.WriteLine("Press any key to invoke GC");
+            Console.ReadKey();
+
+            n1 = null;
+            GC.Collect();
+
+            Console.WriteLine("Press any key to invoke GC");
+            Console.ReadKey();
+
+            GC.Collect();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        public void Root()
+        {
+            shouldExit = false;
+
+            Name n1 = CompleteName;
+
+            thread = new Thread(o =>
+            {
+                Name n2 = (Name) o;
+                Console.WriteLine("Thread started {0}, {1}", n2.First, n2.Last);
+                while (true)
+                {
+                    Thread.Sleep(500);
+                    if (shouldExit)
+                    {
+                        break;
+                    }
+                }
+            });
+
+            thread.Start(n1);
+
+            Thread.Sleep(500);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            shouldExit = true;
+        }
+
+    }
+}
